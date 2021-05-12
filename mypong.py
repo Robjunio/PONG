d21637d0f0820e6d7773d4900a696e1322acc863
Roberto Antônio <74279517+robertoantony32@users.noreply.github.com>
2021-05-12 14:02:31 -0300

    Otimização de algumas funções, e adição de um anuncio de vitória
    Otimização feita na parte de criação dos paddles, e somatório de ponto. A adição do código de vitória, foi implementado no fim do mesmo

--- mypong.py
+++ mypong.py
@@ -7,6 +7,16 @@
 import turtle
 import os
 
+# desenhar raquete
+def draw_paddle(paddle, x, y):
+    return (paddle.speed(),
+            paddle.shape("square"),
+            paddle.color("white"),
+            paddle.shapesize(stretch_wid=5, stretch_len=1),
+            paddle.penup(),
+            paddle.goto(x, y))
+
+
 # desenhar tela
 screen = turtle.Screen()
 screen.title("My Pong")
@@ -14,31 +24,21 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-#desenhar raquete 1
+# variaveis para raquete 1
 paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto(-350,0)
+paddle_1_function = draw_paddle(paddle_1, -350, 0)
 
-#desenhar raquete 2
+# variaveis para raquete 2
 paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
-paddle_2.shape("square")
-paddle_2.color("white")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto(350,0)
+paddle_2_function = draw_paddle(paddle_2, 350, 0)
 
-#desenhar bola
+# desenhar bola
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
 
@@ -54,9 +54,18 @@ hud.color("white")
 hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Press Start 2P",24,"normal") )
+hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
 
 # mover raquete 1
+def paddle_1_down():
+    y = paddle_1.ycor()
+    if y > -250:
+        y += -20
+    else:
+        y = -250
+    paddle_1.sety(y)
+
+
 def paddle_1_up():
     y = paddle_1.ycor()
     if y < 250:
@@ -65,13 +74,6 @@ def paddle_1_up():
         y = 250
     paddle_1.sety(y)
 
-def paddle_1_down():
-    y = paddle_1.ycor()
-    if y > -250:
-        y += -20
-    else:
-        y = -250
-    paddle_1.sety(y)
 
 def paddle_2_up():
     y = paddle_2.ycor()
@@ -89,12 +91,14 @@ def paddle_2_down():
         y = -250
     paddle_2.sety(y)
 
+
 # mapeando as teclas
 screen.listen()
-screen.onkeypress(paddle_1_up,"w")
-screen.onkeypress(paddle_1_down,"s")
-screen.onkeypress(paddle_2_up,"Up")
-screen.onkeypress(paddle_2_down,"Down")
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_1_down, "s")
+screen.onkeypress(paddle_2_up, "Up")
+screen.onkeypress(paddle_2_down, "Down")
+
 
 while True:
     screen.update()
@@ -103,45 +107,76 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    #colisão com parede superior
+    # colisão com parede superior
     if ball.ycor() > 290:
         os.system("afplay bounce.wav&")
         ball.sety(290)
         ball.dy *= -1
     
-    #colisão com parede inferior
+    # colisão com parede inferior
     if ball.ycor() < -280:
         os.system("afplay bounce.wav&")
         ball.sety(-280)
         ball.dy *= -1
 
-    #colisão com parede esquerda
-    if ball.xcor() < -390:
-        score_2 += 1
+    # colisão com paredes laterais
+    if ball.xcor() > 390 or ball.xcor() < -390:
+        if ball.xcor() > 390:
+            score_1 += 1
+        elif ball.xcor() < -390:
+            score_2 += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
+        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
         os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
-        ball.goto(0,0)
+        ball.goto(0, 0)
         ball.dx *= -1
-    
-    #colisão com parede direita
-    if ball.xcor() > 390:
-        score_1 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
-        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
-        ball.goto(0,0)
-        ball.dx *= -1
-
 
     # colisão com raquete 1
+<<<<<<< Updated upstream
     if -370 < ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
         ball.dx *= -1
         ball.setx(-325)
         os.system("afplay bounce.wav&")   
     
+=======
+    if ball.xcor() < -330 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
+        ball.dx *= -1     
+        ball.setx(-325)
+        if ball.ycor() <= paddle_1.ycor() + 50 or ball.ycor() >= paddle_1.ycor() - 50:  # alterei aqui
+            ball.dy *= -1  # alterei aqui
+        os.system("afplay bounce.wav&")
+
+>>>>>>> Stashed changes
     # colisão com raquete 2
     if 370 > ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
         ball.dx *= -1
         ball.setx(325)
+<<<<<<< Updated upstream
         os.system("afplay bounce.wav&")
+=======
+        if ball.ycor() <= paddle_2.ycor() + 50 or ball.ycor() >= paddle_2.ycor() - 50:  # alterei aqui
+            ball.dy *= -1  # alterei aqui
+        os.system("afplay bounce.wav&")
+
+    # Anuncio de vitória
+    time_for_close = 500
+    if score_1 == 11 or score_2 == 11:
+        while time_for_close > 0:
+            letter_win = ''
+            time_for_close -= 1
+            if score_1 == 11:
+                letter_win = '< venceu =)'
+            elif score_2 == 11:
+                letter_win = '=) venceu >>'
+            winner_letter = turtle.Turtle()
+            winner_letter.speed(0)
+            winner_letter.shape("square")
+            winner_letter.color("white")
+            winner_letter.penup()
+            winner_letter.hideturtle()
+            winner_letter.goto(0, 0)
+            winner_letter.write(letter_win, align="center", font=("Press Start 2P", 24, "normal"))
+
+        if time_for_close == 0:
+            break
+>>>>>>> Stashed changes
